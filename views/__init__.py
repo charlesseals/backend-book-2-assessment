187e6746d9918f9550234966bd34c38647e16c4e
Charles Seals <charlesseals11@gmail.com>
2023-01-26 09:24:26 -0600

    added functionality that allows a post request to be sent

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,3 @@
 from .species_requests import get_all_specieses, get_single_species
 
-from .snake_requests import get_all_snakes, get_single_snake
+from .snake_requests import get_all_snakes, get_single_snake, create_snake
